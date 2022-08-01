7ee4711062f828b04a7c7816df844fbeba146d03
Mehady05e <mehadyhasan05@gmail.comm>
2022-08-01 19:02:33 +0600

    end upto question area

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $(".slide-wrap").slick({
+      arrows: true,
+      nextArrow:'.right-btn',
+      prevArrow:'.left-btn',
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      
+      });
+
+});
