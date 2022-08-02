0e43fd673a96517491df9e63d102368f44a333b0
Mehady05e <mehadyhasan05@gmail.comm>
2022-08-02 23:47:32 +0600

    responsive ok

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,29 @@ $(function(){
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
       });
 
       $('.company-slide').slick({
